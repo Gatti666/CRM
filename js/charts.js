02cf3ef0c52278d48442fcece44e6a64a7dedc0b
Gatti666 <natliastephany1@gmail.com>
2026-01-12 23:49:55 -0300

    Descreva aqui as suas alterações

--- js/charts.js
+++ js/charts.js
@@ -1,24 +1,34 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
 
 const data = {
-  labels: ['Dia 01', 'Dia 01', 'Dia 02', 'Dia 03', 'Dia 04', 'Dia 05', 'Dia 06'],
-  datasets: [{
-    label: 'carros',
-    data: [65, 59, 80, 81, 26, 55, 40],
-    borderColor: 'rgb(75, 192, 192)',
-    fill: false
-  }]
+  labels: [
+    "Dia 01",
+    "Dia 01",
+    "Dia 02",
+    "Dia 03",
+    "Dia 04",
+    "Dia 05",
+    "Dia 06",
+  ],
+  datasets: [
+    {
+      label: "carros",
+      data: [65, 59, 80, 81, 26, 55, 40],
+      borderColor: "rgb(75, 192, 192)",
+      fill: false,
+    },
+  ],
 };
 
 const config = {
-  type: 'line',
+  type: "line",
   data: data,
   options: {
-    responsive: true
-  }
+    responsive: true,
+  },
 };
 
 new Chart(ctx, config);
-window.addEventListener('resize', function() {
-    chart.resize();
+window.addEventListener("resize", function () {
+  chart.resize();
 });
--- js/checkbox.js
+++ js/checkbox.js
@@ -1,18 +1,14 @@
- function ocultarLinha(checkbox) {
-            // Confirma se o checkbox está marcado
-            if (checkbox.checked) {
-                // Encontra o elemento pai do checkbox (a célula <td>)
-                var celula = checkbox.parentNode;
-                // Encontra o elemento pai da célula (a linha <tr>)
-                var linha = celula.parentNode;
-                
-                // Opcional: Adicione um atraso para uma transição suave (ex: 300ms)
-                linha.style.transition = "opacity 0.3s ease-out";
-                linha.style.opacity = "0";
+function ocultarLinha(checkbox) {
+  if (checkbox.checked) {
+    var celula = checkbox.parentNode;
 
-                // Remove a linha após a transição (ou imediatamente se preferir)
-                setTimeout(function() {
-                    linha.parentNode.removeChild(linha);
-                }, 300); // 300ms de atraso
-            }
-        }
+    var linha = celula.parentNode;
+
+    linha.style.transition = "opacity 0.3s ease-out";
+    linha.style.opacity = "0";
+
+    setTimeout(function () {
+      linha.parentNode.removeChild(linha);
+    }, 300); // 300ms de atraso
+  }
+}
